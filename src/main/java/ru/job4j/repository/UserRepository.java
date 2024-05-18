d51a736cb3d805e1c44f9d5946650a5357fbb255
Elena Pakulina <el.pakulina@yandex.ru>
2024-05-18 21:39:12 +0500

    2. HQL [#6874]

---
+++ src/main/java/ru/job4j/repository/UserRepository.java
@@ -0,0 +1,163 @@
+package ru.job4j.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import ru.job4j.model.User;
+
+import java.sql.ResultSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+@AllArgsConstructor
+public class UserRepository {
+    private final SessionFactory sf;
+
+    /**
+     * Сохранить в базе.
+     * @param user пользователь.
+     * @return пользователь с id.
+     */
+    public User create(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+        return user;
+    }
+
+    /**
+     * Обновить в базе пользователя.
+     * @param user пользователь.
+     */
+    public void update(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.update(user);
+//            session.createQuery(
+//                            "UPDATE auto_user SET login = :fLogin, password = :fPassword WHERE id = :fId")
+//                    .setParameter("fLogin", user.getLogin())
+//                    .setParameter("fPassword", user.getPassword())
+//                    .setParameter("fId", user.getId())
+//                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+    }
+
+    /**
+     * Удалить пользователя по id.
+     * @param userId ID
+     */
+    public void delete(int userId) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "DELETE User WHERE id = :fId")
+                    .setParameter("fId", userId)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+    }
+
+    /**
+     * Список пользователь отсортированных по id.
+     * @return список пользователей.
+     */
+    public List<User> findAllOrderById() {
+        List<User> answer = new LinkedList<>();
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery(
+                    "from User ORDER BY id ASC", User.class);
+            answer = query.getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+        return answer;
+    }
+
+    /**
+     * Найти пользователя по ID
+     * @return пользователь.
+     */
+    public Optional<User> findById(int userId) {
+        List<User> answer = null;
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery(
+                    "from User where id = :fId", User.class);
+            query.setParameter("fId", userId);
+            answer = query.getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            System.out.println(e.getMessage());
+        }
+        session.close();
+        return Optional.ofNullable(answer.isEmpty() ? null : answer.get(0));
+    }
+
+    /**
+     * Список пользователей по login LIKE %key%
+     * @param key key
+     * @return список пользователей.
+     */
+    public List<User> findByLikeLogin(String key) {
+        List<User> answer = new LinkedList<>();
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery(
+                    "from User where login LIKE :fLogin", User.class);
+            query.setParameter("fLogin", "%" + key + "%");
+            answer = query.getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+        return answer;
+    }
+
+    /**
+     * Найти пользователя по login.
+     * @param login login.
+     * @return Optional or user.
+     */
+    public Optional<User> findByLogin(String login) {
+        List<User> answer = new LinkedList<>();
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery(
+                    "from User where login = :fLogin", User.class);
+            query.setParameter("fLogin", login);
+            answer = query.getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+        return Optional.ofNullable(answer.isEmpty() ? null : answer.get(0));
+    }
+}
